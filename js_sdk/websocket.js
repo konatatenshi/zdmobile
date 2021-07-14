af1d5fe6e29280e2bf8f1ee4fcd1a5f0fcfcfe48
tianshisama <yangqh1230@gmail.com>
2021-07-15 01:32:56 +0800

    板块列表

---
+++ js_sdk/websocket.js
@@ -0,0 +1,105 @@
+class websocketUtil {
+	constructor(url, time) {
+		this.is_open_socket = false //避免重复连接
+		this.url = url //地址
+		this.data = null
+		this.status = null; // websocket是否关闭
+		this.lockReconnect = false //避免重复连接
+		//心跳检测
+		this.timeout= time //多少秒执行检测
+		this.heartbeatInterval= null //检测服务器端是否还活着
+		this.reconnectTimeOut= null //重连之后多久再次重连
+
+		try {
+			return this.connectSocketInit()
+		} catch (e) {
+			console.log('catch');
+			this.is_open_socket = false
+			this.reconnect();
+		}
+	}
+
+	// 进入这个页面的时候创建websocket连接【整个页面随时使用】
+	connectSocketInit() {
+		this.socketTask = uni.connectSocket({
+			url: this.url,
+			success:()=>{
+				console.log("正准备建立websocket中...");
+				// 返回实例
+				return this.socketTask
+			},
+		});
+		this.socketTask.onOpen((res) => {
+			console.log("WebSocket连接正常！");
+			uni.$emit('onsocket', {  
+				data: "on"
+			});
+			clearTimeout(this.reconnectTimeOut)
+			//clearTimeout(this.heartbeatInterval)
+			this.is_open_socket = true;
+			this.start();
+			// 注：只有连接正常打开中 ，才能正常收到消息
+			this.socketTask.onMessage((res) => {
+				uni.$emit('websocketmessage', {  
+				                data: res.data
+				            });
+				uni.$emit('websocketmessage2', {  
+				                data: res.data
+				            });
+				console.log(res.data)
+			});
+		}) 
+		// 监听连接失败，这里代码我注释掉的原因是因为如果服务器关闭后，和下面的onclose方法一起发起重连操作，这样会导致重复连接
+		// uni.onSocketError((res) => {
+		// 	console.log('WebSocket连接打开失败，请检查！');
+		// 	this.is_open_socket = false;
+		// 	this.reconnect();
+		// });
+		// 这里仅是事件监听【如果socket关闭了会执行】
+		this.socketTask.onClose(() => {
+			console.log("已经被关闭了")
+			this.is_open_socket = false;
+			this.reconnect();
+		})
+	}
+	
+	//发送消息
+	send(value){
+		// 注：只有连接正常打开中 ，才能正常成功发送消息
+		this.socketTask.send({
+			data: value,
+			async success() {
+				console.log("消息发送成功");
+			},
+		});
+	}
+	//开启心跳检测
+	start(){
+		this.heartbeatInterval = setTimeout(()=>{
+			this.data=[{cmd:"ping"}]
+			console.log(this.data)
+			this.send(JSON.stringify(this.data));
+			clearTimeout(this.heartbeatInterval);
+			this.start();
+		},10000)
+	}
+	//重新连接
+	reconnect(){
+		// 防止多个方法调用，多处重连
+		if (this.lockReconnect) {
+			return;
+		};
+		this.lockReconnect = true;
+		//停止发送心跳
+		clearInterval(this.heartbeatInterval)
+		//如果不是人为关闭的话，进行重连
+		if(!this.is_open_socket){
+			this.reconnectTimeOut = setTimeout(()=>{
+				this.connectSocketInit();
+				this.lockReconnect = false;
+			},3000)
+		}
+	}
+}
+
+module.exports = websocketUtil
---
+++ pages/basics/forum.vue
@@ -0,0 +1,329 @@
+<template name="basics">
+	<view>
+		<cu-custom class="statustop" bgColor="bg-gradual-pink" :isBack="true">
+			<block slot="backText">返回</block>
+			<block slot="content">{{forumname}}</block>
+		</cu-custom>
+		<scroll-view scroll-x class="bg-white nav-sm" :style="'margin-top: -' + iStatusBarHeight +'px;'"
+			scroll-with-animation :scroll-left="scrollLeft">
+			<view class="cu-item" :class="index==TabCur?'text-green cur':''" v-for="(item,index) in 7" :key="index"
+				@tap="tabSelect" :data-id="index">
+				{{tabname[index]}}
+			</view>
+		</scroll-view>
+		<view class="view_head">
+			<swiper class="swiper-box" :style="'height: ' + swiperheight +'px;'" :current="TabCur" @change="tabChange">
+				<swiper-item key="1">
+					<scroll-view class="list">
+						<view v-if="1 > 0">
+							<!-- 	图文列表
+							<block v-for="(item,index1) in items.list" :key="index1">
+								<view>{{item}}</view>
+							</block> -->
+						</view>
+					</scroll-view>
+				</swiper-item>
+				<swiper-item key="2">
+					<scroll-view class="list">
+						<view class="view_listnow">
+							<view v-if="threadlist.length > 0">
+								<block v-for="(item,index2) in threadlist" :key="index2" @tap="tourl(item.tid)">
+									<view class="solid-bottom text-df"
+										style="padding-top: 10upx; padding-bottom: 10upx;" v-if="item.displayorder>0">
+										<view>
+											<text class="text-black text-cut"
+												style="width: 100%;">{{item.subject}}</text>
+										</view>
+										<view> <text class="text-sm text-red padding-sm">置顶</text> <text
+												class="text-sm text-gray padding-sm">{{item.author}}&nbsp&nbsp{{item.replies}}评</text>
+										</view>
+									</view>
+									<view class="cu-card article no-card" v-else>
+										<view class="cu-card dynamic solid-bottom">
+											<view class="cu-item shadow">
+												<view class="cu-list menu-avatar">
+													<view class="cu-item">
+														<view class="cu-avatar round lg" :style="[{ backgroundImage:'url(' + item.userinfo.avatarlist + ')' }]"></view>
+														<view class="content flex-sub">
+															<view>{{item.author}}</view>
+															<view class="text-gray text-sm flex justify-between">
+																{{item.nowdate}}
+															</view>
+														</view>
+													</view>
+												</view>
+												<view class="forumtitle text-cut">{{item.subject}}</view>
+												<view class="text-content">
+														{{item.summary}}
+												</view>
+												<view v-if="item.attachment==2 && item.attachinfo.length == 1" class="grid flex-sub padding-lr col-1">
+													<view class="bg-img only-img" :style="[{ backgroundImage:'url(' + item.attachlist[0].attachment + ')' }]">
+													</view>
+												</view>
+												<view v-else-if="item.attachment==2 && item.attachinfo.length == 2" class="grid flex-sub padding-lr col-3 grid-square">
+													<view class="bg-img" :style="[{ backgroundImage:'url(' + item.attachlist[0].attachment + ')' }]">
+													</view>
+													<view class="bg-img" :style="[{ backgroundImage:'url(' + item.attachlist[1].attachment + ')' }]">
+													</view>
+												</view>
+												<view v-else-if="item.attachment==2 && item.attachinfo.length > 2" class="grid flex-sub padding-lr col-3 grid-square">
+													<view class="bg-img" :style="[{ backgroundImage:'url(' + item.attachlist[0].attachment + ')' }]">
+													</view>
+													<view class="bg-img" :style="[{ backgroundImage:'url(' + item.attachlist[1].attachment + ')' }]">
+													</view>
+													<view class="bg-img" :style="[{ backgroundImage:'url(' + item.attachlist[2].attachment + ')' }]">
+													</view>
+												</view>
+												<view class="text-gray text-sm text-right padding">
+													<text class="cuIcon-attentionfill margin-lr-xs"></text> {{item.views}}
+													<text class="cuIcon-appreciatefill margin-lr-xs"></text> {{item.recommends}}
+													<text class="cuIcon-messagefill margin-lr-xs"></text> {{item.replies}}
+												</view>
+											</view>
+										</view>
+									</view>
+								</block>
+							</view>
+							<view v-if="newpost.length > 0">
+								<block v-for="(item,index3) in newpost" :key="index3" @tap="tourl(item.url)">
+									<view class="solid-bottom cu-card article no-card">
+										<view class="cu-item shadow">
+											<view class="title">
+												<view class="text-cut">{{item.title}}</view>
+											</view>
+											<view class="content">
+												<image v-if="item.img != 'static/image/common/nophoto.gif'"
+													:src="item.img" mode="aspectFill"></image>
+												<view class="desc">
+													<view class="text-content">
+														{{item.summary}}
+													</view>
+													<view>
+														<view class="cu-tag bg-red light sm round">{{item.author}}
+														</view>
+														<view class="cu-tag bg-green light sm round">{{item.replies}}评
+														</view>
+													</view>
+												</view>
+											</view>
+										</view>
+									</view>
+								</block>
+							</view>
+							<view>
+								<block>
+									<view class="padding-xs flex align-center bg-gray">
+										<view class="flex-sub text-center">
+											<view class="text-xs padding">
+												<text class="text-black">{{loading}}</text>
+											</view>
+										</view>
+									</view>
+								</block>
+							</view>
+						</view>
+					</scroll-view>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="cu-modal" :class="modalName=='needlogin'?'show':''">
+			<view class="cu-dialog">
+				<view class="cu-bar bg-white justify-end">
+					<view class="content">需要登录</view>
+					<view class="action" @tap="hideModal">
+						<text class="cuIcon-close text-red"></text>
+					</view>
+				</view>
+				<view class="padding-xl">
+					你需要登录才可以使用此功能。
+				</view>
+				<view class="cu-bar bg-white justify-end">
+					<view class="action">
+						<button class="cu-btn line-green text-green" @tap="hideModal">取消</button>
+						<button class="cu-btn bg-green margin-left" @tap="tologin">确定</button>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="cu-modal" :class="modalName=='cantview'?'show':''">
+			<view class="cu-dialog">
+				<view class="cu-bar bg-white justify-end">
+					<view class="content">权限不足</view>
+					<view class="action" @tap="hideModal">
+						<text class="cuIcon-close text-red"></text>
+					</view>
+				</view>
+				<view class="padding-xl">
+					无法进入板块，错误提示：{{cantviewmessage}}
+				</view>
+				<view class="cu-bar bg-white justify-end">
+					<view class="action">
+						<button class="cu-btn line-green text-green" @tap="hideModal">取消</button>
+						<button class="cu-btn bg-green margin-left" @tap="tologin">确定</button>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "basics",
+		data() {
+			return {
+				scrollLeft: 0,
+				forumname: "载入中……",
+				cantviewmessage: "unknown",
+				iStatusBarHeight: 0,
+				swiperList: [],
+				threadlist: [],
+				threadlisttitle: [],
+				newpost: [],
+				tabname: ["新帖", "全部", "热门", "精华"],
+				avatarimgLoaded: false,
+				iscard: false,
+				modalName: null,
+				TabCur: 1,
+				forumid: 2,
+				loading: "上拉可加载更多帖子",
+				swiperheight: 1000, //高度
+			};
+		},
+		methods: {
+			hideModal(e) {
+				this.modalName = null
+			},
+			tologin(e) {
+				uni.navigateTo({
+					url: '../../components/ay-login/login-password'
+				});
+			},
+			tourl(e) {
+				uni.navigateTo({
+					url: '../../components/ay-login/login-password'
+				});
+			},
+			tothebottom() {
+				console.log("到底了");
+			},
+			setHeight(e) {
+				var query = uni.createSelectorQuery();
+				query.select('.' + e).boundingClientRect(rect => {
+					if (rect) {
+						//console.log(e + ".height = " + rect.height)
+						this.swiperheight = rect.height; //页面可见区域 - 头部高度
+						//console.log("this.height = " + this.swiperheight)
+					}
+				}).exec();
+
+			},
+			InputFocus(e) {
+				this.InputBottom = e.detail.height
+			},
+			InputBlur(e) {
+				this.InputBottom = 0
+			},
+			onSuccessImg() {
+				this.avatarimgLoaded = true;
+			},
+			loadthread(forumid, page, orderby, dateline, filter, typeid) {
+				let that = this;
+				uni.request({
+					url: getApp().globalData.zddomain + 'plugin.php?id=ts2t_qqavatar:thread', //获取置顶帖子
+					method: 'GET',
+					timeout: 10000,
+					data: {
+						token: that.$token,
+						forumid: forumid,
+						page: page,
+						orderby: orderby,
+						dateline: dateline,
+						filter: filter,
+						typeid: typeid
+					},
+					header: {
+						'content-type': 'application/x-www-form-urlencoded' //自定义请求头信息
+					},
+					success: (res) => {
+						if (res.data.code == 404) {
+							that.modalName = "needlogin";
+						} else if (res.data.code == 401) {
+							that.modalName = "cantview";
+							that.cantviewmessage = res.data.message;
+						} else {
+							that.threadlist = res.data.post;
+							that.threadlisttitle = res.data.postinfo;
+							that.forumname = res.data.foruminfo1.name;
+							console.log(that.threadlist);
+							setTimeout(function() {
+								that.setHeight("view_listnow");
+							}, 100)
+						}
+					}
+				});
+			},
+			tabSelect(e) {
+				this.TabCur = e.currentTarget.dataset.id;
+				this.scrollLeft = (e.currentTarget.dataset.id - 1) * 60;
+				console.log(this.swiperheight)
+			},
+			tabChange(e) {
+				//this.setHeight();
+				this.currentIndex = e.detail.current;
+				this.TabCur = e.detail.current;
+				this.loadthread(this.forumid);
+			},
+			tourl(e) {
+				console.log(this.swiperheight)
+			}
+		},
+		onLoad: function(option) { //option为object类型，会序列化上个页面传递的参数 
+			this.forumid = option.forumid;
+			console.log(option.forumid); //打印出上个页面传递的参数。
+			this.loadthread(this.forumid);
+			this.iStatusBarHeight = uni.getSystemInfoSync().statusBarHeight;
+		},
+		mounted() {
+			//this.setHeight();
+		}
+	}
+</script>
+
+<style>
+	.cu-card>.cu-item {
+		background-color: #f0f0f0!important;
+		margin: 0;
+	}
+	.cu-list.menu-avatar>.cu-item{
+		background-color: #f0f0f0;
+		height: 110upx;
+	}
+	.page {
+		height: 100vh;
+	}
+
+	.nav-sm {
+		white-space: nowrap;
+	}
+
+	.nav-sm .cu-item.cur {
+		border-bottom: 2px solid;
+	}
+
+	.nav-sm .cu-item {
+		height: 30px;
+		display: inline-block;
+		line-height: 30px;
+		margin: 0 5px;
+		padding: 0 11px;
+	}
+	
+	.forumtitle{
+	    font-size: 15px;
+	    font-weight: 900;
+	    color: #333333;
+	    line-height: 20px;
+	    padding: 0 15px;
+	}
+</style>
